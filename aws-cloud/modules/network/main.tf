f98e30fac8a5a5ffb6a964e5d1ad9effc0f5627d
frank <frank@thoughtmachine.net>
2026-04-22 00:25:37 +0800

    AWS Endpoints in private subnets; remove NAT

--- aws-cloud/modules/network/main.tf
+++ aws-cloud/modules/network/main.tf
@@ -2,6 +2,9 @@ locals {
   vpc_name = "${var.project}-vpc"
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.main.names, 0, 3)
+  endpoint_services = [
+    "eks", "ecr.api", "ecr.dkr", "ec2", "sts"
+  ]
 }
 
 data "aws_availability_zones" "main" {
@@ -17,7 +20,7 @@ module "vpc" {
   public_subnets     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
   private_subnets    = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 3)]
   database_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 6)]
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   single_nat_gateway = true
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
@@ -34,5 +37,29 @@ resource "aws_route53_zone" "main" {
   vpc {
     vpc_id     = module.vpc.vpc_id
     vpc_region = var.aws_region
+  }
+}
+
+# AWS Interface Endpoints for private subnets
+resource "aws_vpc_endpoint" "interface" {
+  for_each            = toset(local.endpoint_services)
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.${each.value}"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnets
+  private_dns_enabled = true
+  tags = {
+    Name    = "${var.project}-${each.value}"
+  }
+}
+
+# S3 Gateway Endpoint for private subnets
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = module.vpc.private_route_table_ids
+  tags = {
+    Name    = "${var.project}-s3"
   }
 }
